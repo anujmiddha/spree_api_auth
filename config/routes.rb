fabe521c79f7e50f89b60fe11394cc540e27e677
Anuj <anuj.middha@gmail.com>
2017-03-27 12:57:02 +0530

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
-Spree::Core::Engine.routes.prepend do
-  namespace :api do
-    scope :module => :v1 do
-      namespace :users do
-        post   :sign_up
-        post   :sign_in
-      end
+Spree::Core::Engine.add_routes do
+  namespace :api, defaults: {format: 'json'} do
+    resource :users do
+      post :sign_up
+      post :sign_in
     end
   end
 end
